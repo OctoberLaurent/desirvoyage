13a72e4d11c11b92894064fe084cf0e8a5824b37
gregory <greg@MacBook-Pro-de-gregory.local>
2020-01-30 23:43:13 +0100

    requete get

--- assets/js/postalcode.js
+++ assets/js/postalcode.js
@@ -1,19 +1,88 @@
 $(document).ready(function(){
-    $( "#target" ).on( "keypress", function() {
-        clearTimeout(this, 'timer');
-  var wait = setTimeout(search, 500);
-  $(this).data('timer', wait);
+  
+
+    $( "#target" ).on( "keyup", function() {
+        clearTimeout($.data(this, 'timer'));
+        var wait = setTimeout(search, 500);
+        $(this).data('timer', wait);
+        
     });
-    function search() {
-        $.post("user.php", {nStr: "" + $('#target').val() + ""}, function(data){
-          if(data.length > 0) {
-            $('#suggestions').show();
-            $('#autoSuggestionsList').html(data);
-          }else{
-            $('#suggestions').hide();
-          }
-        });
-        console.log('target');
-    };
-    
+    $('input.autocomplete').autocomplete();
+
   });
+  //var elems = document.getElementById("target").value;
+  //var instance = M.Adresse.getInstance(elem);
+function search(){
+    var autocomplete = document.getElementById("target");
+    
+
+
+    let query = autocomplete.value;
+    let limit = "1";
+
+
+
+    //console.log(query);
+    
+    $.ajax({
+        method: "GET",
+        url: "/api/address",
+        data: { q: query }
+      })
+        .done(function( results ) {    
+            
+            let autodata = {};
+            let autopost = {};
+            results = JSON.parse( results );
+            //console.log(results.features[0].properties.postcode);  
+            
+            for( let i in results.features ){
+                let row = results.features[ i ];
+                 //console.log( row );
+                autodata[ row.properties.label] = null;
+                autopost[ row.properties.postcode] = null;
+                
+
+            }
+
+           
+         //console.log( autodata );
+        $('#target').autocomplete("updateData", autodata);
+
+        });
+
+
+            
+ $( '#target' ).change(function() {
+
+ query = document.getElementById("target").value; 
+
+ 
+ $.ajax({
+     method: "GET",
+     url: "/api/postal",
+     data: { q: query, limit: limit }
+   })
+
+
+   .done(function( results_post ) { 
+    results_post = JSON.parse(results_post);
+    var Postcode = document.getElementById("code");
+    var result_final = results_post.features[0].properties.postcode;
+    Postcode.value = result_final;
+   
+    // console.log(results_post.features[0].properties.postcode);
+
+})
+
+
+
+
+ 
+ 
+     
+
+   });
+
+
+}
