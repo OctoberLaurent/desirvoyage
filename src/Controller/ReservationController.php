ffacf8c7600395daec2c969fa7da9d18d7f31019
gregory <greg@MacBook-Pro-de-gregory.local>
2020-02-06 11:04:09 +0100

    evolution purchase travel

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,18 +2,93 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StaysRepository;
+use App\Repository\TravelRepository;
+use Doctrine\ORM\Query\Expr\Func;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Node\Expression\FunctionExpression;
 
 class ReservationController extends AbstractController
 {
     /**
-     * @Route("/reservation", name="reservation")
+     * @Route("/reservation", name="reservation_index")
      */
-    public function index()
+    public function index(SessionInterface $session, TravelRepository $travelRepository)
     {
+        $reservation = $session->get('reservation',[]);
+
+        $reservationWithData = [];
+
+        foreach($reservation as $id => $quantity)
+        {
+            $reservationWithData[] = [
+                'travel' => $travelRepository->find($id),
+                'quantity' => $quantity
+
+            ];
+        }
+        //dd($reservationWithData);
         return $this->render('reservation/index.html.twig', [
-            'controller_name' => 'ReservationController',
+            'items' => $reservationWithData
         ]);
+    }
+
+    /**
+     * @route("/reservation/add/{id}", name="reservation_add")
+     */
+    public function add($id, SessionInterface $session)
+    {
+        
+       
+        $reservation = $session->get('reservation', []);
+
+        if(!empty($reservation[$id]))
+        {
+            //quantité +1 si tu clique 
+            $reservation[$id]++;
+        }else{
+
+            //quantité de base
+            $reservation[$id] = 1;
+        }
+        
+
+        $session->set('reservation', $reservation);
+
+        //dd($session->get('reservation'));
+        return $this->redirectToRoute("reservation_index");
+    }
+
+    /**
+     * @route("/reservation/configure", name="reservation_configure")
+     */
+    public Function configure()
+    {
+        
+
+        return $this->render('reservation/configureTravel.html.twig', [
+            
+        ]);
+
+    }
+
+    /**
+     * @route("/reservation/remove/{id}", name= "reservation_remove")
+     */
+    public Function remove($id, SessionInterface $session)
+    {
+        $reservation = $session->get('reservation', []);
+
+        if(!empty($reservation[$id]))
+        {
+            unset($reservation[$id]);
+        }
+
+        $session->set('reservation', $reservation);
+
+        return $this->redirectToRoute("reservation_index");
     }
 }
---
+++ src/Migrations/Version20200206100139.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200206100139 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE stays ADD stock INT NOT NULL');
+        $this->addSql('ALTER TABLE reservation DROP stock');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE reservation ADD stock INT NOT NULL');
+        $this->addSql('ALTER TABLE stays DROP stock');
+    }
+}
