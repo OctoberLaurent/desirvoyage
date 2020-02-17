46f61a65cd4df00a8611cadeb76dbba429d568a6
Laurent <laurent@lepl.at>
2020-02-17 10:31:06 +0100

    Bug

--- assets/js/dropDown.js
+++ assets/js/dropDown.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
+    
     // DropDown
     $(".dropdown-trigger").dropdown();
+    
     // modal
     $('.modal').modal();
+
+    // for responsive sidebar
+    $('.sidenav').sidenav();
 
 }); 
