dce9c8af249e471e7e4c7afdbe37a25314e79c9d
laurent <laurent@lepl.at>
2026-07-13 18:12:01 +0200

    fix(travel): render single images without carousel

--- tests/Configuration/TravelImageFallbackTemplateTest.php
+++ tests/Configuration/TravelImageFallbackTemplateTest.php
@@ -11,7 +11,17 @@ final class TravelImageFallbackTemplateTest extends TestCase
         $template = (string) file_get_contents(dirname(__DIR__, 2).'/templates/travel/showone.html.twig');
 
         self::assertStringContainsString(
+            '{% if travel.pictures|length < 2 %}',
+            $template,
+        );
+
+        self::assertStringContainsString(
             '<img class="responsive-img travel-image-fallback" src="/data2/default.png" alt="{{ travel.name }}">',
+            $template,
+        );
+
+        self::assertStringContainsString(
+            '<img class="responsive-img travel-image-fallback" src="{{ picture.picturename }}" alt="{{ picture.name }}" onerror="this.onerror=null; this.src=\'/data2/default.png\';">',
             $template,
         );
     }
